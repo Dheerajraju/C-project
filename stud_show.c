78ef0481ea2b6a546963b8e54c9baa37967fa5ae
Dheeraj raju <dheshal07@gmail.com>
2025-11-04 22:14:56 +0530

    Update stud_show.c

--- stud_show.c
+++ stud_show.c
@@ -1,28 +1,40 @@
-#include"header.h"
+// sub function show particular record
+#include "header.h"
+
 void show(ST *ptr)
 {
-        ST *temp=ptr;
-        printf(" _______________________________________________________\n");
-        printf("|                                                       |\n");
-        printf("|       %-3d       |      %-13s   |    %6.2f    |\n",temp->roll,temp->name,temp->percentage);
-        printf("|                                                       |\n");
-        printf("|_______________________________________________________|\n");
+    if (ptr == NULL)
+    {
+        printf("No record to show\n");
+        return;
+    }
+    
+    printf(" ____________________________________\n");
+    printf("| Roll | Name          | Percentage |\n");
+    printf("|______|_______________|____________|\n");
+    printf("| %-4d |     %-13s     |   %6.2f    |\n", ptr->roll, ptr->name, ptr->percentage);
+    printf("|______|_______________|____________|\n");
 }
 
-
+// main function for show the records
 void stud_show(ST *ptr)
 {
-        ST *temp=ptr;
-        printf(" _______________________________________________________\n");
-        printf("|                                                       |\n");
-        while(temp)
-        {
-                printf("|       %-3d       |      %-13s   |    %6.2f    |\n",temp->roll,temp->name,temp->percentage);
-                temp=temp->next;
-        }
-        printf("|                                                       |\n");
-        printf("|_______________________________________________________|\n");
-
+    if (ptr == NULL)
+    {
+        printf("No records to display\n");
+        return;
+    }
+    ST *temp = ptr;
+    printf(" ____________________________________\n");
+    printf("| Roll | Name          | Percentage |\n");
+    printf("|______|_______________|____________|\n");
+    
+    while(temp)
+    {
+        printf("| %-4d | %-13s | %6.2f     |\n", temp->roll, temp->name, temp->percentage);
+        temp = temp->next;
+    }
+    
+    printf("|______|_______________|____________|\n");
 }
-
 
